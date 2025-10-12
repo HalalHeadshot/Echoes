332291807e5dcddf57826ec6bf6ef71c392b9110
Sohan Bhadalkar <chimpbowl502@gmail.com>
2025-10-12 23:22:22 +0530

    (FRONTEND) timeline page major ui

--- geotag-app/client/src/pages/HomePage.jsx
+++ geotag-app/client/src/pages/HomePage.jsx
@@ -8,8 +8,8 @@ import { useTheme } from "../context/ThemeContext";
 const HomePage = () => {
   const { dark, setDark } = useTheme();
   return (
-    <div className="w-[100vw] h-[100vh] bg-main dark:bg-dmain p-[10px]">
-    <div className="h-[100%] w-[100%] relative overflow-hidden rounded-lg border-[1.5px] border-borderColor dark:border-dborderColor">
+    <div className="w-[100vw] h-[100vh] bg-main dark:bg-dmain">
+    <div className="h-[100%] w-[100%] relative overflow-hidden">
      
       <div className="gradientContainerTop absolute z-[900] h-[50px] bg-[linear-gradient(to_bottom,theme(colors.fadeColor)_0%,transparent_100%)] 
     dark:bg-[linear-gradient(to_bottom,theme(colors.dfadeColor)_0%,transparent_100%)] top-0 w-[100%]"></div>
